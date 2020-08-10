313ee4801f7b84e7706389e00bb19f14f087e43c
JohnMajorsAcc <gitMyCodeBitch12>
2020-08-10 13:03:23 +0700

    netology

--- src/com/company/task311/FromBorrowedStatusMover.java
+++ src/com/company/task311/FromBorrowedStatusMover.java
@@ -1,16 +1,5 @@
 package com.company.task311;
 
 public class FromBorrowedStatusMover extends BookMover {
-//    @Override
-//    public void moveToStatus(Book book, Status requestedStatus) {
-//        switch (requestedStatus){
-//            case ARCHIVED:
-//            case OVERDUED:
-//            case AVAILABLE:{
-//                System.out.println("Статус книги переведен на \"" + requestedStatus.getDescription() + "\"");
-//                book.setStatus(requestedStatus);
-//                break;
-//            }
-//        }
-//    }
+
 }
--- src/com/company/task311/FromOverduedStatusMover.java
+++ src/com/company/task311/FromOverduedStatusMover.java
@@ -1,16 +1,5 @@
 package com.company.task311;
 
 public class FromOverduedStatusMover extends BookMover {
-//    @Override
-//    public void moveToStatus(Book book, Status requestedStatus) {
-//        switch (requestedStatus) {
-//            case AVAILABLE:
-//            case ARCHIVED: {
-//                System.out.println("Статус книги переведен на \"" + requestedStatus.getDescription() + "\"");
-//                book.setStatus(requestedStatus);
-//                break;
-//
-//            }
-//        }
-//    }
+
 }
